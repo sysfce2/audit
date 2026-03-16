5727e7025b3207a35131f7a02aa870974ecc9e59
Steve Grubb <ausearch.1@gmail.com>
2026-03-16 18:28:03 -0400

    Correct auplugin_fgets tests
    Due to a misunderstanding of the auplugin_mem enum meanings, the tests were constructed wrong. This patch correctly uses the setvbuf API.

--- auplugin/test/fgets_r_test.c
+++ auplugin/test/fgets_r_test.c
@@ -12,14 +12,12 @@ static void test_basic_state(void)
 {
 	int fds[2];
 	char buf[16];
-	char custom[32];
 	auplugin_fgets_state_t *st;
 
 	assert(pipe(fds) == 0);
 
 	st = auplugin_fgets_init();
 	assert(st);
-	assert(auplugin_setvbuf_r(st, custom, sizeof(custom), MEM_SELF_MANAGED) == 0);
 
 	/* no data yet */
 	assert(auplugin_fgets_more_r(st, sizeof(buf)) == 0);
@@ -48,35 +46,48 @@ static void test_deferred_compaction(void)
 {
 	int fds[2];
 	char buf[64];
-	char custom[33];
+	char *custom;
 	const char *line =
 		"0123456789abcdef0123456789abcdefQRSTUVWX\n";
 	auplugin_fgets_state_t *st;
 	size_t line_len = strlen(line);
-	size_t capacity = sizeof(custom) - 1;
+	size_t capacity = 33;
 
 	assert(pipe(fds) == 0);
 	st = auplugin_fgets_init();
 	assert(st);
-	assert(auplugin_setvbuf_r(st, custom, capacity, MEM_SELF_MANAGED) == 0);
+	custom = malloc(capacity);
+	assert(custom);
+	assert(auplugin_setvbuf_r(st, custom, capacity, MEM_MALLOC) == 0);
 
 	assert(write(fds[1], line, line_len) == (ssize_t)line_len);
 	close(fds[1]);
 
 	int len = auplugin_fgets_r(st, buf, sizeof(buf), fds[0]);
-	assert(len == (int)capacity);
+	assert(len == (int)(capacity - 1));
 	assert(strncmp(buf, line, (size_t)len) == 0);
 	assert(auplugin_fgets_eof_r(st) == 0);
 
 	len = auplugin_fgets_r(st, buf, sizeof(buf), fds[0]);
-	assert(len == (int)(line_len - capacity));
-	assert(strcmp(buf, line + capacity) == 0);
+	assert(len == (int)(line_len - (capacity - 1)));
+	assert(strcmp(buf, line + (capacity - 1)) == 0);
 
 	len = auplugin_fgets_r(st, buf, sizeof(buf), fds[0]);
 	assert(len == 0);
 	assert(auplugin_fgets_eof_r(st) == 1);
 
 	close(fds[0]);
+	auplugin_fgets_destroy(st);
+}
+
+static void test_reject_self_managed_override(void)
+{
+	auplugin_fgets_state_t *st;
+	char custom[32];
+
+	st = auplugin_fgets_init();
+	assert(st);
+	assert(auplugin_setvbuf_r(st, custom, sizeof(custom), MEM_SELF_MANAGED) == 1);
 	auplugin_fgets_destroy(st);
 }
 
@@ -122,8 +133,8 @@ int main(void)
 {
 	test_basic_state();
 	test_deferred_compaction();
+	test_reject_self_managed_override();
 	test_mmap_file();
 	printf("audit-fgets_r tests: all passed\n");
 	return 0;
 }
-
